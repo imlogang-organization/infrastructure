044fd5754fccf340725922b16b0385dc481c58cb
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-15 00:06:05 -0500

    try underscore

--- Kubernetes/Secrets/container-runner-cloud/variables.tf
+++ Kubernetes/Secrets/container-runner-cloud/variables.tf
@@ -1,9 +1,9 @@
-variable "imlogang-organization-container-runner" {
+variable "imlogang_organization_container_runner" {
   description = "Auth Token of imlogang-organization Container Runner"
   type        = string
 }
 
-variable "logan-container-runner" {
+variable "logan_container_runner" {
   description   = "Auth Token of logan Container Runner"
   type          = string
 }
