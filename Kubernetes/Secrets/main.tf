2b06ba610d9962209ff7f5979a885b6d9b9f3c58
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-14 19:59:38 -0500

    fix variiables.tf

--- Kubernetes/Secrets/main.tf
+++ Kubernetes/Secrets/main.tf
@@ -1,24 +1,24 @@
-resource "kubernetes_namespace" "beemoviebot" {
-  metadata {
-    name = "beemoviebot"
-  }
-}
+# resource "kubernetes_namespace" "beemoviebot" {
+#   metadata {
+#     name = "beemoviebot"
+#   }
+# }
 
-resource "kubernetes_secret" "production-secrets" {
-  metadata {
-    name      = "production-secrets"
-    namespace = kubernetes_namespace.beemoviebot.metadata[0].name
-  }
+# resource "kubernetes_secret" "production-secrets" {
+#   metadata {
+#     name      = "production-secrets"
+#     namespace = kubernetes_namespace.beemoviebot.metadata[0].name
+#   }
 
-  data = {
-    CLIENT_ID = var.client_id_prod
-    BEEMOVIEBOT = var.beemoviebot
-    PteroToken = var.pterotoken
-  }
+#   data = {
+#     CLIENT_ID = locals.client_id
+#     BEEMOVIEBOT = locals.beemoviebot
+#     PteroToken = locals.pterotoken
+#   }
 
-  type = "Opaque"
+#   type = "Opaque"
 
-  depends_on = [
-    kubernetes_namespace.beemoviebot,
-  ]
-}
+#   depends_on = [
+#     kubernetes_namespace.beemoviebot,
+#   ]
+# }
