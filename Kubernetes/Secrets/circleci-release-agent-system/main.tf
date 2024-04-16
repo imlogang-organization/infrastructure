6c4e70c9e932d0e77a2166450f0041c650983a18
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-16 01:27:49 -0500

    change circleci release secret name

--- Kubernetes/Secrets/circleci-release-agent-system/main.tf
+++ Kubernetes/Secrets/circleci-release-agent-system/main.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_secret" "circleci-release-agent-system" {
   metadata {
-    name      = "token"
+    name      = "circleci-release-agent-system"
     namespace = kubernetes_namespace.circleci-release-agent-system.metadata[0].name
   }
 
