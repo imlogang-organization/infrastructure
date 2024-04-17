bb48ebe5bb074a7c6ca8f7893f75cc6f9f935de1
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-17 16:15:46 -0500

    new minio bucket

--- minio/main.tf
+++ minio/main.tf
@@ -2,3 +2,8 @@ resource "minio_s3_bucket" "state_terraform_s3" {
   bucket = "ci-state-file"
   acl    = "public"
 }
+
+resource "minio_s3_bucket" "g_drive_backup" {
+  bucket = "g-drive-backup"
+  acl    = "public" 
+}
