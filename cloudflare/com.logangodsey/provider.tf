7c7e4d372039c974ef9bb0345008ae30556db3b7
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-17 17:16:50 -0500

    add cloudflare folder for websites to do eventually

---
+++ cloudflare/com.logangodsey/provider.tf
@@ -0,0 +1,14 @@
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
+# need to see if there's a way to import
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  # Configuration options
+}
