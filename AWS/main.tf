e92c97c1a784deb6fae9835b496479871d7637d2
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-18 17:19:58 -0500

    create new security group and assign to runner IP

--- AWS/main.tf
+++ AWS/main.tf
@@ -20,5 +20,28 @@ resource "aws_instance" "runner_host" {
     tags = {
         Name = "Runner-Host"  
     }
-    vpc_security_group_ids = [ "sg-02a7bd12867aff59c" ]
+    vpc_security_group_ids = [ aws_security_group.runner.id ]
+}
+
+resource "aws_security_group" "runner" {
+  name        = "runner"
+  description = "Allow all Runner"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Runner"
+  }
 }
