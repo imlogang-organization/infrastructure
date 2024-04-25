244ff5e5eca42f583d4a499dc9ab8f7c68599e0a
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-25 15:33:44 -0500

    try with local-exec

--- Kubernetes/Helm/container-agent/main.tf
+++ Kubernetes/Helm/container-agent/main.tf
@@ -1,7 +1,12 @@
-resource "helm_repository" "container_agent" {
-  name = "container-agent"
-  url  = "https://packagecloud.io/circleci/container-agent/helm"
+resource "null_resource" "add_helm_repo" {
+  provisioner "local-exec" {
+    command = "helm repo add container-agent https://packagecloud.io/circleci/container-agent/helm"
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 }
+
 resource "helm_release" "container_agent" {
   name       = var.deployment_name
   namespace  = var.namespace
@@ -10,5 +15,9 @@ resource "helm_release" "container_agent" {
 
   values = [
     file("${path.module}/values/normal-values.yaml") // This will refer to Kubernetes/Helm/values.yaml
+  ]
+  
+  depends_on = [
+    null_resource.add_helm_repo
   ]
 }
