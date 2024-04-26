1b99bb428d948d4d88a967bedcf644741dbff9e3
imlogang <37604784+imlogang@users.noreply.github.com>
2024-04-25 23:52:42 -0500

    create ptero oats bucket

--- minio/main.tf
+++ minio/main.tf
@@ -7,3 +7,8 @@ resource "minio_s3_bucket" "g_drive_backup" {
   bucket = "g-drive-backup"
   acl    = "public" 
 }
+
+resource "minio_s3_bucket" "ptero_oats_backup" {
+  bucket = "ptero_oats_backup"
+  acl    = "public" 
+}
