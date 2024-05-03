05da33b2935e35f3fce184ee978738bf5e387543
imlogang <37604784+imlogang@users.noreply.github.com>
2024-05-03 00:35:06 -0500

    initial commit of container-agent-server

---
+++ Kubernetes/Helm/container-agent-server/variables.tf
@@ -0,0 +1,19 @@
+variable "namespace" {
+    description = "Namespace of Container Agent for Server"
+    default = "container-runner-server"
+}
+
+variable "deployment_name" {
+    description = "Deployment name."
+    default = "container-runner-server"
+}
+
+variable "chart_name" {
+    description = "Chart Name."
+    default = "container-agent/container-agent"
+}
+
+# variable "chart_version" {
+#     description = "Version to Pull from Chart."
+#     default = "101.0.21"
+# }
