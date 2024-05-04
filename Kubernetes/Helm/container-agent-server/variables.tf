8300799cb8c9faa8739cea099738a9a119de52f5
imlogang <37604784+imlogang@users.noreply.github.com>
2024-05-04 00:21:25 -0500

    re-release

--- Kubernetes/Helm/container-agent-server/variables.tf
+++ Kubernetes/Helm/container-agent-server/variables.tf
@@ -12,6 +12,7 @@ variable "chart_name" {
     description = "Chart Name."
     default = "container-agent/container-agent"
 }
+
 # variable "chart_version" {
 #     description = "Version to Pull from Chart."
 #     default = "101.0.21"
