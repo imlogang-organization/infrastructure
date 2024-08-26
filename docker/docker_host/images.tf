4a0584df8e33c8431223cc18405a3b3730f7f77a
imlogang <37604784+imlogang@users.noreply.github.com>
2024-08-26 18:55:37 -0500

    add tautulli, homearr, dokuwiki, and uptime-kuma

--- docker/docker_host/images.tf
+++ docker/docker_host/images.tf
@@ -9,3 +9,19 @@ resource "docker_image" "homeassistant" {
 resource "docker_image" "watchtower" {
   name = "containrrr/watchtower"
 }
+
+resource "docker_image" "tautulli" {
+  name = "lscr.io/linuxserver/tautulli:latest"
+}
+
+resource "docker_image" "homearr" {
+  name = "ghcr.io/ajnart/homarr:latest"
+}
+
+resource "docker_image" "dokuwiki" {
+  name = "lscr.io/linuxserver/dokuwiki:latest"
+}
+
+resource "docker_image" "uptime-kuma" {
+  name = "uptime-kuma louislam/uptime-kuma:1"
+}
--- docker/docker_host/main.tf
+++ docker/docker_host/main.tf
@@ -56,3 +56,87 @@ resource "docker_container" "watchtower" {
   }
   restart = var.restart
 }
+
+resource "docker_container" "tautulli" {
+  name  = "tautulli"
+  image = docker_image.tautulli.name
+  env = [
+    "PUID=1000",
+    "PGID=1000",
+    "TZ=Etc/CDT"
+  ]
+  ports {
+    internal = 8181
+    external = 8181
+  }
+  volumes {
+    container_path = "/config"
+    host_path      = "${var.home_directory}/tautulli/config"
+  }
+  restart = var.restart
+}
+
+resource "docker_container" "homearr" {
+  name  = "homearr"
+  image = docker_image.homearr.name
+  env = [
+    "PUID=1000",
+    "PGID=1000",
+    "TZ=America/Chicago",
+  ]
+  ports {
+    internal = 7575
+    external = 7575
+  }
+  volumes {
+    container_path = "/var/run/docker.sock"
+    host_path      = "/var/run/docker.sock"
+  }
+  volumes {
+    container_path = "/app/data/configs"
+    host_path      = "${var.home_directory}/homarr/configs"
+  }
+  volume {
+    container_path = "/app/public/icons"
+    host_path      = "${var.home.directory}/homarr/icon"
+  }
+  restart = var.restart
+}
+
+resource "docker_container" "dokuwiki" {
+  name  = "dokuwiki"
+  image = docker_image.dokuwiki.name
+  env = [
+    "PUID=1000",
+    "PGID=1000",
+    "TZ=ACDT/UTC",
+  ]
+  ports {
+    internal = 80
+    external = 3003
+  }
+  volumes {
+    container_path = "/config"
+    host_path      = "${var.home_directory}/dokuwiki/config"
+  }
+  restart = var.restart
+}
+
+resource "docker_container" "uptime-kuma" {
+  name  = "uptime-kuma"
+  image = docker_image.dokuwiki.name
+  env = [
+    "PUID=1000",
+    "PGID=1000",
+    "TZ=ACDT/UTC",
+  ]
+  ports {
+    internal = 3001
+    external = 3001
+  }
+  volumes {
+    container_path = "/app/data"
+    host_path      = "${var.home_directory}/uptime-kuma"
+  }
+  restart = var.restart
+}
