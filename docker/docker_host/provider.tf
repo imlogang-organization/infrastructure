ee018f7f4aaaaed53083154d15228c480800a517
imlogang <37604784+imlogang@users.noreply.github.com>
2024-08-26 17:38:13 -0500

    update provider to be correct

--- docker/docker_host/provider.tf
+++ docker/docker_host/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     docker = {
-      #https://registry.terraform.io/providers/calxus/docker/latest/docs/resources/image
-      source = "calxus/docker"
+      #https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs/resources/container
+      source = "kreuzwerker/docke"
       version = "3.0.2"
     }
   }
