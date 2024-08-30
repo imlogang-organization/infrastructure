00d8d70601e0039cc6b1bcfdf1a6e58f69775185
Logan <37604784+imlogang@users.noreply.github.com>
2024-08-30 14:58:58 -0500

    try adding the volumes back?

--- docker/docker_host/volumes.tf
+++ docker/docker_host/volumes.tf
@@ -1,7 +1,7 @@
-# resource "docker_volume" "grafana-storage" {
-#   name = "grafana-storage"
-# }
+resource "docker_volume" "grafana-storage" {
+  name = "grafana-storage"
+}
 
-# resource "docker_volume" "influxdb-storage" {
-#   name = "influxdb-stroage"
-# }
+resource "docker_volume" "influxdb-storage" {
+  name = "influxdb-stroage"
+}
