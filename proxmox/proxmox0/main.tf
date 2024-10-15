c64e3e842274b5d7567fc809139535ecb33e485c
Logan <37604784+imlogang@users.noreply.github.com>
2024-10-14 19:17:34 -0500

    try and make proxmox vms

--- proxmox/proxmox0/main.tf
+++ proxmox/proxmox0/main.tf
@@ -3,10 +3,10 @@ resource "proxmox_vm_qemu" "mrserver" {
   desc = "Machine Runner for Self Hosted CircleCI Server."
   os_type = "ubuntu"
   target_node = "proxmox0"
-  clone = ""
-  storage = ""
+  clone = "ubuntu24.04-server-template"
+  storage = "local-lvm"
   cores = 4
-  memory = 16384
+  memory = 8192
   skip_ipv6 = true
   disk {
     slot     = 0
@@ -21,8 +21,8 @@ resource "proxmox_vm_qemu" "nomad_client" {
   desc = "Nomad Client for Self Hosted CircleCI Server."
   os_type = "ubuntu"
   target_node = "proxmox0"
-  clone = ""
-  storage = ""
+  clone = "ubuntu24.04-server-template"
+  storage = "local-lvm"
   cores = 6
   memory = 10240
   skip_ipv6 = true
