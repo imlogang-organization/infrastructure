3575fb56cc4c68ec3d286e6af5ddf321b64138f1
imlogang <37604784+imlogang@users.noreply.github.com>
2024-10-20 14:38:02 -0500

    delete ubuntu-k8s-7

--- proxmox/proxmox0/main.tf
+++ proxmox/proxmox0/main.tf
@@ -42,24 +42,3 @@ resource "proxmox_vm_qemu" "nomad_client" {
   }
 }
 
-resource "proxmox_vm_qemu" "ubuntu-k8s-7" {
-  name = "ubuntu-k8s-7"
-  desc = "Part of the Control plane for my Kubernetes Cluster so we can get HA plus have a node tainted for only CI workloads."
-  vmid = 104
-  os_type = var.os_type
-  target_node = var.target_node
-  clone = var.os_template
-  cores = 4
-  memory = 10240
-  skip_ipv6 = true
-  disk {
-    slot     = "scsi0"
-    size     = "150G"
-    type     = "disk"
-    storage  = "local-lvm"
-  }
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-}
