0da8cfe8046a36d67aa928fc8f469ca321c3f625
imlogang <37604784+imlogang@users.noreply.github.com>
2024-10-20 14:28:10 -0500

    add new k8s node to proxmox

--- proxmox/proxmox0/main.tf
+++ proxmox/proxmox0/main.tf
@@ -2,8 +2,8 @@ resource "proxmox_vm_qemu" "mrserver" {
   name = "ubuntu-machine-runner-server"
   desc = "Machine Runner for Self Hosted CircleCI Server."
   vmid = 101
-  os_type = "ubuntu"
-  target_node = "proxmox0"
+  os_type = var.os_type
+  target_node = var.target_node
   clone = "ubuntu24.04-server-template"
   cores = 4
   memory = 8192
@@ -24,10 +24,32 @@ resource "proxmox_vm_qemu" "nomad_client" {
   name = "ubuntu-nomad-client"
   desc = "Nomad Client for Self Hosted CircleCI Server."
   vmid = 102
-  os_type = "ubuntu"
-  target_node = "proxmox0"
+  os_type = var.os_type
+  target_node = var.target_node
   clone = "ubuntu24.04-server-template"
   cores = 6
+  memory = 10240
+  skip_ipv6 = true
+  disk {
+    slot     = "scsi0"
+    size     = "150G"
+    type     = "disk"
+    storage  = "local-lvm"
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+}
+
+resource "proxmox_vm_qemu" "ubuntu-k8s-7" {
+  name = "ubuntu-k8s-7"
+  desc = "Part of the Control plane for my Kubernetes Cluster so we can get HA plus have a node tainted for only CI workloads."
+  vmid = 104
+  os_type = var.os_type
+  target_node = var.target_node
+  clone = var.os_template
+  cores = 4
   memory = 10240
   skip_ipv6 = true
   disk {
--- proxmox/proxmox0/variables.tf
+++ proxmox/proxmox0/variables.tf
@@ -13,3 +13,21 @@ variable "proxmox0_api_url" {
   type = string
   default = "https://proxmox0.logangodsey.com:8006/api2/json"
 }
+
+variable "os_type" {
+  description = "Typical OS Type"
+  type = string
+  default = "ubuntu"
+}
+
+variable "target_node" {
+  description = "Target node"
+  type = string
+  default = "proxmox0"
+}
+
+variable "os_template" {
+  description = "Default Template to create VMs on."
+  type = string
+  default = "ubuntu-24-04-server-template"
+}
