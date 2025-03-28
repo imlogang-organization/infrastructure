b414e1ddc6ad4ebab9dae424157357ae8bf4f78b
imlogang <37604784+imlogang@users.noreply.github.com>
2025-03-28 16:58:07 -0500

    add regcred to go-api namespace to provide pull rate limits

--- Kubernetes/Secrets/go-api/main.tf
+++ Kubernetes/Secrets/go-api/main.tf
@@ -16,3 +16,25 @@ resource "kubernetes_secret" "go-api" {
     kubernetes_namespace.go-api,
   ]
 }
+
+resource "kubernetes_secret" "docker_pull" {
+  metadata {
+    name      = "regcred"
+    namespace =  kubernetes_namespace.go-api.metadata[0].name
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "https://index.docker.io/v1/" = {
+          username = var.docker-username
+          password = var.docker-password
+          email    = var.docker-email
+          auth     = base64encode("${var.docker-username}:${var.docker-password}")
+        }
+      }
+    })
+  }
+}
--- Kubernetes/Secrets/go-api/variables.tf
+++ Kubernetes/Secrets/go-api/variables.tf
@@ -12,3 +12,18 @@ variable "pg_password" {
   description = "Password for Postgres"
   type = string
 }
+
+variable "docker-username" {
+  description = "Docker username"
+  type = string
+}
+
+variable "docker-password" {
+  description = "Docker PAT"
+  type = string
+}
+
+variable "docker-email" {
+  description = "Docker email"
+  type = string
+}
