e4af0db28bbd63a8ccb693488421f2f4ce3bb6c3
imlogang <37604784+imlogang@users.noreply.github.com>
2025-03-28 17:12:58 -0500

    add the same regcred to beemoviebotstage

--- Kubernetes/Secrets/beemoviebotstage/main.tf
+++ Kubernetes/Secrets/beemoviebotstage/main.tf
@@ -17,3 +17,25 @@ resource "kubernetes_secret" "staging-secrets" {
     kubernetes_namespace.beemoviebotstage,
   ]
 }
+
+resource "kubernetes_secret" "docker_pull" {
+  metadata {
+    name      = "regcred"
+    namespace =  kubernetes_namespace.beemoviebotstage.metadata[0].name
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "https://index.docker.io/v1/" = {
+          username = var.docker-username
+          password = var.docker-password
+          email    = var.docker-email
+          auth     = base64encode("${var.docker-username}:${var.docker-password}")
+        }
+      }
+    })
+  }
+}
--- Kubernetes/Secrets/beemoviebotstage/variables.tf
+++ Kubernetes/Secrets/beemoviebotstage/variables.tf
@@ -17,3 +17,18 @@ variable "circle_token" {
   description = "CCI Server API Token"
   type = string
 }
+
+variable "docker-username" {
+  description = "Docker username"
+  type = string
+}
+
+variable "docker-password" {
+  description = "Docker PAT"
+  type = string
+}
+
+variable "docker-email" {
+  description = "Docker email"
+  type = string
+}
