e50aec29ea7fe026f43db08c247e7b0ae1303ae3
imlogang <37604784+imlogang@users.noreply.github.com>
2025-03-28 17:07:02 -0500

    add pull secrets to beemoviebot namespace

--- Kubernetes/Secrets/beemoviebot/main.tf
+++ Kubernetes/Secrets/beemoviebot/main.tf
@@ -17,3 +17,25 @@ resource "kubernetes_secret" "production-secrets" {
     kubernetes_namespace.beemoviebot,
   ]
 }
+
+resource "kubernetes_secret" "docker_pull" {
+  metadata {
+    name      = "regcred"
+    namespace = kubernetes_namespace.beemoviebot.metadata[0].name
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "https://index.docker.io/v1/" = {
+          username = var.docker-username
+          password = var.docker-password
+          email    = var.docker-email
+          auth     = base64encode("${var.docker-username}:${var.docker-password}")
+        }
+      }
+    })
+  }
+}
