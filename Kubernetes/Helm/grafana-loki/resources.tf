22ceeab5d600e62988638019037b4884f574181a
imlogang <37604784+imlogang@users.noreply.github.com>
2025-06-05 17:18:00 -0500

    redeploy grafan-loki in the correct ns

--- Kubernetes/Helm/grafana-loki/resources.tf
+++ Kubernetes/Helm/grafana-loki/resources.tf
@@ -1,5 +1,5 @@
 resource "kubernetes_namespace" "circleci" {
   metadata {
-    name = "grafana-tempo"
+    name = "grafana-loki"
   }
 }
--- Kubernetes/Helm/grafana-loki/variables.tf
+++ Kubernetes/Helm/grafana-loki/variables.tf
@@ -1,17 +1,17 @@
 variable "namespace" {
     description = "Namespace of the deployment"
-    default = "grafana-tempo"
+    default = "grafana-loki"
     type = string
 }
 
 variable "deployment_name" {
     description = "Deployment name."
-    default = "grafana-tempo"
+    default = "grafana-loki"
     type = string
 }
 
 variable "chart_name" {
     description = "Chart Name."
-    default = "grafana/tempo"
+    default = "grafana/loki"
     type = string
 }
